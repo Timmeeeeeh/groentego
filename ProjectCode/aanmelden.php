96168495e9b4d72f0d38c486a8802dc57a0ce9b1
Jeroen <jeroen-h@hotmail.nl>
2017-02-22 13:39:40 +0100

    De aanmelden.php is verbeterd, de css etc is aangepast aan de nieuwe map structuur

--- ProjectCode/aanmelden.php
+++ ProjectCode/aanmelden.php
@@ -3,7 +3,8 @@ $school_name = $_POST["school_naam"];
 $aantal_leerlingen = $_POST["aantal_leerlingen"];
 $datum = $_POST["datum"];
 $opmerkingen = $_POST["Opmerkingen"];
-require_once('db_connection.php');
+$status = TRUE ;
+require_once('Aanmelden/db_connection.php');
 
 
 //Check to see if the server connects properly
@@ -12,18 +13,17 @@ $connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 $sql = "INSERT INTO groepen (School_naam, Aantal_leerlingen, Datum, Opmerkingen) VALUES ('$school_name', '$aantal_leerlingen', '$datum', '$opmerkingen')";
 
 if ($connection->query($sql) === TRUE) {
-    echo "Je hebt succeful een cursus aangemeld!";
+    $status = TRUE;
 } else {
-    echo "Error: " . $sql . "<br>" . $connection->error;
+    $status = FALSE ;
 }
 
 $connection -> close();
-
 ?>
 
 <html lang="en">
-    <link rel="stylesheet" type="text/css" href="index.css/CSS" media="screen">
-    <link rel="shortcut icon" href="/HTML/IMG/logo.png" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="Aanmelden/index.css" media="screen">
+    <link rel="shortcut icon" href="Aanmelden/logo.png" type="image/x-icon">
 <head>
     <meta charset="UTF-8">
     <title>Aanmelden Cursus</title>
@@ -54,5 +54,18 @@ $connection -> close();
         </li>
     </ul>
 </div>
+
+<div class="message">
+    <p>
+        Uw aanmelding is:  <?php echo $status; ?> <br>
+        U heeft uw school aangemeld met de volgende gegevens:<br>
+        School: <?php echo $school_name; ?><br>
+        Aantal leerlingen: <?php echo $aantal_leerlingen; ?><br>
+        Datum: <?php echo $datum; ?><br>
+        Opmerkingen: <?php echo $opmerkingen; ?><br>
+    </p>
+</div>
 </body>
+
 </html>
+
